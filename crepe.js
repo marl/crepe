e9e07f5837abf86328d16b40efebe183e38efbfc
Jong Wook Kim <jongwook@nyu.edu>
2018-04-09 05:37:10 -0400

    showing pitch only when voicing confidence > 0.25, doing normalization, code cleanup

--- crepe.js
+++ crepe.js
@@ -1,5 +1,5 @@
 !function(window) {
-  var audioContext = new AudioContext();
+  const audioContext = new AudioContext();
   document.getElementById('srate').innerHTML = audioContext.sampleRate
 
   function error(message) {
@@ -12,23 +12,24 @@
   }
 
   function resample(audioBuffer, onComplete) {
-    if (audioBuffer.sampleRate == 48000) {
-      var buffer = audioContext.createBuffer(audioBuffer.numberOfChannels, 1024, 16000);
+    if (audioBuffer.sampleRate % 16000 == 0) {
+      const multiplier = audioBuffer.sampleRate / 16000;
+      const buffer = audioContext.createBuffer(audioBuffer.numberOfChannels, 1024, 16000);
       for (var c = 0; c < audioBuffer.numberOfChannels; c++) {
-        var original = audioBuffer.getChannelData(c);
-        var destination = buffer.getChannelData(c);
+        const original = audioBuffer.getChannelData(c);
+        const destination = buffer.getChannelData(c);
 
         for (var i = 0; i < 1024; i++) {
-          destination[i] = original[i * 3];
+          destination[i] = original[i * multiplier];
         }
       }
       onComplete(buffer);
     } else {
-      var channel = audioBuffer.numberOfChannels;
-      var samples = audioBuffer.length * 16000 / audioBuffer.sampleRate;
+      const channel = audioBuffer.numberOfChannels;
+      const samples = audioBuffer.length * 16000 / audioBuffer.sampleRate;
 
-      var offlineContext = new OfflineAudioContext(channel, samples, 16000);
-      var bufferSource = offlineContext.createBufferSource();
+      const offlineContext = new OfflineAudioContext(channel, samples, 16000);
+      const bufferSource = offlineContext.createBufferSource();
       bufferSource.buffer = audioBuffer;
 
       bufferSource.connect(offlineContext.destination);
@@ -39,35 +40,39 @@
     }
   }
 
-  var cent_mapping = tf.add(tf.linspace(0, 7180, 360), tf.tensor(1997.3794084376191))
+  const cent_mapping = tf.add(tf.linspace(0, 7180, 360), tf.tensor(1997.3794084376191))
 
   function process_microphone_buffer(event) {
-    var buffer = event.inputBuffer
+    resample(event.inputBuffer, function(resampled) {
+      tf.tidy(() => {
+        const frame = tf.tensor(resampled.getChannelData(0).slice(0, 1024));
+        const mean = tf.mean(frame)
+        const zeromean = tf.sub(frame, mean);
+        const framenorm = tf.norm(zeromean)
+        const framestd = tf.tensor(framenorm.dataSync()/Math.sqrt(1024))
+        const normalized = tf.div(zeromean, framestd);
+        const input = normalized.reshape([1, 1024])
+        const output = model.predict([input])
+        const salience = output.reshape([360]);
+        const confidence = salience.max().dataSync()[0];
+        const argmax = salience.argMax().dataSync()[0];
 
-    resample(buffer, function(resampled) {
-      var frame = resampled.getChannelData(0).slice(0, 1024);
-      var salience = model.predict([tf.tensor(frame).reshape([1, 1024])]).reshape([360])
+        const start = Math.max(0, argmax - 4);
+        const end = Math.min(360, argmax + 5);
 
-      // cut the frequency of at 1150 Hz, because there aren't really much data trained for these high pitches
-      var cutoff = 300
-      salience = salience.slice([0], [cutoff]);
+        const weights = salience.slice([start], [end - start]);
+        const cents = cent_mapping.slice([start], [end - start]);
+        const products = tf.mul(weights, cents)
+        const productSum = tf.sum(products).dataSync()[0]
+        const weightSum = tf.sum(weights).dataSync()[0]
+        const predicted_cent = productSum / weightSum;
+        const predicted_hz = 10 * Math.pow(2, predicted_cent / 1200.0);
+        const result = (confidence > 0.25) ? predicted_hz.toFixed(3) + ' Hz' : 'no voice';
 
-      var confidence = salience.max().dataSync()[0];
-      var argmax = salience.argMax().dataSync()[0];
-
-      var start = Math.max(0, argmax - 4);
-      var end = Math.min(cutoff, argmax + 5);
-
-      salience = salience.slice([start], [end - start]);
-      cents = cent_mapping.slice([start], [end - start]);
-      productSum = tf.sum(tf.mul(salience, cents)).dataSync()[0]
-      weightSum = tf.sum(salience).dataSync()[0]
-      predicted_cent = productSum / weightSum;
-      predicted_hz = 10 * Math.pow(2, predicted_cent / 1200.0);
-
-      document.getElementById('estimated-pitch').innerHTML = predicted_hz.toFixed(3);
-      document.getElementById('voicing-confidence').innerHTML = confidence.toFixed(3);
-    })
+        document.getElementById('estimated-pitch').innerHTML = result;
+        document.getElementById('voicing-confidence').innerHTML = confidence.toFixed(3);
+      });
+    });
   }
 
   function initAudio() {
@@ -83,15 +88,15 @@
       navigator.getUserMedia({audio: true}, function(stream) {
         status('Setting up AudioContext ...');
         console.log('Audio context sample rate = ' + audioContext.sampleRate);
-        var mic = audioContext.createMediaStreamSource(stream);
+        const mic = audioContext.createMediaStreamSource(stream);
 
-        var minBufferSize = audioContext.sampleRate / 16000 * 1024;
+        const minBufferSize = audioContext.sampleRate / 16000 * 1024;
         for (var bufferSize = 4; bufferSize < minBufferSize; bufferSize *= 2);
         console.log('Buffer size = ' + bufferSize);
-        var scriptNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
+        const scriptNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
         scriptNode.onaudioprocess = process_microphone_buffer;
 
-        var gain = audioContext.createGain();
+        const gain = audioContext.createGain();
         gain.gain.setTargetAtTime(0, audioContext.currentTime, 0);
 
         mic.connect(scriptNode);
