d7625cabb776d7631aa332a74ab0cc2cc1989ca3
Jong Wook Kim <jongwook@nyu.edu>
2018-04-18 10:10:11 -0600

    added cli test

--- tests/test_sweep.py
+++ tests/test_sweep.py
@@ -2,14 +2,12 @@ import os
 import numpy as np
 import crepe
 
+# this data contains a sine sweep
+file = os.path.join(os.path.dirname(__file__), 'sweep.wav')
+f0_file = os.path.join(os.path.dirname(__file__), 'sweep.f0.csv')
 
-def test_sweep():
-    # this data contains a sine sweep
-    file = os.path.join(os.path.dirname(__file__), 'sweep.wav')
 
-    crepe.process_file(file)
-    f0_file = os.path.join(os.path.dirname(__file__), 'sweep.f0.csv')
-
+def verify_f0():
     result = np.loadtxt(f0_file, delimiter=',', skiprows=1)
 
     # it should be confident enough about the presence of pitch in every frame
@@ -17,3 +15,15 @@ def test_sweep():
 
     # the frequencies should be linear
     assert np.corrcoef(result[:, 1]) > 0.99
+
+    os.remove(f0_file)
+
+
+def test_sweep():
+    crepe.process_file(file)
+    verify_f0()
+
+
+def test_sweep_cli():
+    assert os.system("crepe {}".format(file)) == 0
+    verify_f0()
