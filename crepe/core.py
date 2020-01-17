fix #49, normalization bug casued by in-memory ops

crepe/core.py
@@ -202,7 +202,7 @@ def get_activation(audio, sr, model_capacity='full', center=True, step_size=10,
     n_frames = 1 + int((len(audio) - 1024) / hop_length)
     frames = as_strided(audio, shape=(1024, n_frames),
                         strides=(audio.itemsize, hop_length * audio.itemsize))
-    frames = frames.transpose()
+    frames = frames.transpose().copy()
 
     # normalize each frame -- this is expected by the model
     frames -= np.mean(frames, axis=1)[:, np.newaxis]